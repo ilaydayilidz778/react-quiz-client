46e5596e7492f3a5c6a91eda86afc89c4d071246
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-03-03 00:15:59 +0300

    This project is an API Quiz project developed using React and ASP.NET Core 8. Mui Material Design Components are utilized on the React side. Within the project, the design of the user login form and form validation processes have been completed.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,33 +1,52 @@
-import { Box, Button, Card, CardContent, TextField } from '@mui/material'
+import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import React from 'react'
+import Center from '../center/Center/'
+import useForm from '../../hooks/useForm';
+
+const getFreshModelObject = () => ({
+    name: "",
+    email: ""
+});
 
 export default function Login() {
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange
+    } = useForm();
+
     return (
-        <Card sx={{ width: 400 }}>
-            <CardContent>
-                <Box sx={{
-                    '& .MuiTextField-root': {
-                        m: 1,
-                        width: '90%'
-                    }
-                }}>
-                    <from noValidate autoComplete="off">
-                        <TextField
-                            label="Email"
-                            name="email"
-                            variant="outlined" />
-                        <TextField
-                            label="Name"
-                            name="name"
-                            variant="outlined" />
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            size="large"
-                            sx={{ width: "90%" }}>Start</Button>
-                    </from>
-                </Box>
-            </CardContent>
-        </Card>
-    )
+        <Center>
+            <Card sx={{ width: 400 }}>
+                <CardContent sx={{ textAlign: "center" }}>
+                    <Typography variant="h3" sx={{ my: 3 }}>Quiz App</Typography>
+                    <Box sx={{
+                        '& .MuiTextField-root': {
+                            m: 1,
+                            width: '90%'
+                        }
+                    }}>
+                        <form noValidate autoComplete="off">
+                            <TextField
+                                label="Email"
+                                name="email"
+                                value={ }
+                                variant="outlined" />
+                            <TextField
+                                label="Name"
+                                name="name"
+                                variant="outlined" />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                sx={{ width: "90%" }}>Start</Button>
+                        </form>
+                    </Box>
+                </CardContent>
+            </Card>
+        </Center>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
